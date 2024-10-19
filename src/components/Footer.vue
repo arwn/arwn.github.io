5890a2b385299838948e98f2904ffefceb9d012b
arwn <windham.aren@protonmail.com>
2024-10-18 20:26:19 -0500

    Add footer and update about

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<script setup>
+const datestr = new Date().toLocaleDateString("en-US", {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+});
+</script>
+
+<style scoped>
+footer {
+  height: 44px;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  margin-bottom: 0.5rem;
+  margin-left: 1rem;
+}
+</style>
+
+<template>
+  <footer>
+    <p>
+      © {{ datestr }} <i>Aren Windham</i> [<a href="about">about</a>] [<a
+        href="contact"
+        >contact</a
+      >]
+    </p>
+  </footer>
+</template>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,3 @@
-
 <template>
-  <p>
-    <a href="/">home</a> | <a href="/blog">blog</a> | <a href="/about">about</a>
-  </p>
+  <p><a href="/">home</a></p>
 </template>
