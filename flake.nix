5777e730e180fd711607a849a2b938b2f53d4c69
arwn <windham.aren@protonmail.com>
2024-10-22 14:58:31 -0500

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs=[
+            pkgs.bun
+            #pkgs.nodePackages.typescript
+            #pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      }
+    );
+}
